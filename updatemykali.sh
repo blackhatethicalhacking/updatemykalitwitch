a4e50ceb71db13faecfe97b885f136de7646d348
Black Hat Ethical Hacking <13942386+blackhatethicalhacking@users.noreply.github.com>
2021-08-28 22:10:53 +0300

    Create updatemykali.sh

---
+++ updatemykali.sh
@@ -0,0 +1,14 @@
+figlet Security is a myth! | lolcat
+sleep 0.5
+echo "Your Kali Linux will now start updating!!" | lolcat
+sleep 0.5
+echo "Starting in 3 seconds..." | lolcat
+sleep 1
+echo "Starting in 2 seconds..." | lolcat
+sleep 1
+echo "Starting in 1 second..." | lolcat
+sleep 1
+echo "Updating!" | lolcat
+sleep 0.5
+apt-get update -y && apt-get full-upgrade -y && apt-get dist-upgrade -y && apt autoremove -y && apt>
+echo "Update has been completed, Enjoy!" | lolcat
